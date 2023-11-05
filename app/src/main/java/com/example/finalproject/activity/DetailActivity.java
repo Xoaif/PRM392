a9d32c5fc77d662e4f8e5e4036aba1f09752195a
hung <bigmuppig@gmail.com>
2023-11-05 15:57:56 +0700

    list product after add new

--- app/src/main/java/com/example/finalproject/activity/DetailActivity.java
+++ app/src/main/java/com/example/finalproject/activity/DetailActivity.java
@@ -2,15 +2,33 @@ package com.example.finalproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.finalproject.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView detailDescription;
+    TextView detailName;
+    ImageView detailImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        detailDescription = findViewById(R.id.detailDescription);
+        detailImage = findViewById(R.id.detailImage);
+        detailName = findViewById(R.id.detailName);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            detailDescription.setText(bundle.getString("Description"));
+            detailName.setText(bundle.getString("Name"));
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/adaper/ProductAdapter.java
+++ app/src/main/java/com/example/finalproject/adaper/ProductAdapter.java
@@ -1,2 +1,80 @@
-package com.example.finalproject.adaper;public class ProductAdapter {
+package com.example.finalproject.adaper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.finalproject.R;
+import com.example.finalproject.activity.DetailActivity;
+import com.example.finalproject.entity.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private Context context;
+    private List<Product> dataList;
+
+    public ProductAdapter(Context context, List<Product> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Glide.with(context).load(dataList.get(position).getImage()).into(holder.recImage);
+        holder.recName.setText(dataList.get(position).getProductName());
+        holder.recPrice.setText(dataList.get(position).getPrice());
+        holder.recDescription.setText(dataList.get(position).getDescription());
+
+        holder.recCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("Image", dataList.get(holder.getAdapterPosition()).getProductName());
+                intent.putExtra("Description", dataList.get(holder.getAdapterPosition()).getDescription());
+                intent.putExtra("Price", dataList.get(holder.getAdapterPosition()).getPrice());
+
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
+class MyViewHolder extends RecyclerView.ViewHolder{
+
+    ImageView recImage;
+    TextView recName, recPrice, recDescription;
+    CardView recCard;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recImage = itemView.findViewById(R.id.recImage);
+        recCard = itemView.findViewById(R.id.recCard);
+        recDescription = itemView.findViewById(R.id.recDescription);
+        recPrice = itemView.findViewById(R.id.recPrice);
+        recName = itemView.findViewById(R.id.recName);
+
+    }
 }
